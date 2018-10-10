6f5a41cc9906188cabb1df7579208541fc291238
itwill <itwill@itwill-PC>
2018-10-10 18:03:38 +0900

    서영진 DTO Push

---
+++ BeauDamProject/src/main/java/com/exe/dto/BasketDTO.java
@@ -0,0 +1,85 @@
+package com.exe.dto;
+
+public class BasketDTO {
+
+	private int basketNum;
+
+    private String id;
+
+    private String code;
+
+    private String productName;
+
+    private String colorCode;
+
+    private int productPrice;
+
+    private int qty;
+
+    public int getBasketNum() {
+        return basketNum;
+    }
+
+    public void setBasketNum(int basketNum) {
+        this.basketNum = basketNum;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getColorCode() {
+        return colorCode;
+    }
+
+    public void setColorCode(String colorCode) {
+        this.colorCode = colorCode;
+    }
+
+    public int getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(int productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
