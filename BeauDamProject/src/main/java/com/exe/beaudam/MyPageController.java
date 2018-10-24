a1147a47879154fea7d7daecc724979cc1990eee
estebanguitar <esteban1009@naver.com>
2018-10-24 09:30:24 +0900

    divide controller

---
+++ BeauDamProject/src/main/java/com/exe/beaudam/MyPageController.java
@@ -0,0 +1,103 @@
+package com.exe.beaudam;
+
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+/*
+ * 	쿼리 insert, delete 테이블 순서
+ * 	
+ * 	insert 할 때
+ * 	member - member_Info - member_grade - coupon
+ * 	product - brand - img - color - basket  
+ * 	admin테이블 - brand
+ * 	delete 할 때 insert 역순
+ * 	
+ * 
+ * 	리뷰 delete 할 때 sale_code로 id 검증 후 삭제 시킬 것
+ * 	
+ * 	notification delete는 num만 있으면 삭제 되므로 관리자 계정일 때만
+ * 	삭제버튼 활성화	
+ * 
+ * 	DB insert 필독
+ * 	디폴트 값이 있어 추가하지 않아도 되는 데이터
+ * 	
+ * 	Member 관련 컬럼
+ * 	- isLeave, created, pay, grade, point, num, period, use_date
+ * 
+ * 	Sale 관련 컬럼
+ * 	- saleDate, pay_status, delivery_status
+ * 
+ * 	Notificaiton 관련 컬럼
+ * 	- num, id, hitCount, created
+ * 
+ * 	Review 관련 컬럼
+ *  - created  
+ *
+ *	DB update 필독
+ *	파라미터 타입이 다들 다르니 사용 전 사용할 Mapper를 확인할 것
+ *
+ */
+@Controller
+public class MyPageController {
+	
+
+	// ********************** My Page **********************
+
+	@RequestMapping(value = "/myPage.action", method = { RequestMethod.GET, RequestMethod.POST })
+	public String myPage() {
+
+		// 마이페이지 이동
+		return "myPage/myPage";
+	}	
+
+	@RequestMapping(value = "/myBasket.action", method = { RequestMethod.GET, RequestMethod.POST })
+	public String myBasket() {
+
+		// 장바구니(마이페이지) 페이지 이동
+
+		return "myPage/myBasket";
+	}
+
+	@RequestMapping(value = "/myInfo.action", method = RequestMethod.GET)
+	public String myInfo() {
+
+		// 마이페이지 비밀번호확인 페이지 이동
+		return "myPage/myInfo";
+	}
+
+	@RequestMapping(value = "/myEdit.action", method = { RequestMethod.GET, RequestMethod.POST })
+	public String myEdit() {
+
+		// 회원정보 수정2(마이페이지) 페이지 이동
+		return "myPage/myEdit";
+
+	}
+
+	// msj
+	@RequestMapping(value = "/myCoupon.action", method = { RequestMethod.GET, RequestMethod.POST })
+	public String myCoupon() {
+
+		// 보유쿠폰(마이페이지) 페이지 이동
+
+		return "myPage/myCoupon";
+	}
+
+	// msj
+	@RequestMapping(value = "/myOrder.action", method = { RequestMethod.GET, RequestMethod.POST })
+	public String myOrder() {
+
+		// 주문정보 (마이페이지) 페이지 이동
+
+		return "myPage/myOrder";
+	}
+
+	// msj
+	@RequestMapping(value = "/myLeave.action", method = { RequestMethod.GET, RequestMethod.POST })
+	public String myLeave() {
+
+		// 회원탈퇴 (마이페이지) 페이지 이동
+
+		return "myPage/myLeave";
+	}
+	
+}
